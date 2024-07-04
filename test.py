5a47dfb4b787e8b52079127fc44661769b348e88
Dean Sun <deansun0518@gmail.com>
2024-07-03 21:33:04 -0700

    Add hello_world.py, my_gh_workflow.py, and update test.py

--- test.py
+++ test.py
@@ -1,11 +1,17 @@
 import httpx
+from prefect import flow, get_run_logger
 
-def get_repo_info():
-    url = "https://api.github.com/repos/PrefectHQ/prefect"
+
+@flow(retries=3, retry_delay_seconds=5,log_prints=True)
+def get_repo_info(repo_name: str = "PrefectHQ/prefect"):
+    url = f"https://api.github.com/repos/{repo_name}"
     response = httpx.get(url)
+    response.raise_for_status()
     repo = response.json()
-    print("PrefectHQ/prefect repository statistics 🤓:")
+    print(f"{repo_name} repository statistics 🤓:")
     print(f"Stars 🌠 : {repo['stargazers_count']}")
+    print(f"Forks 🍴 : {repo['forks_count']}")
+
 
 if __name__ == "__main__":
-    get_repo_info()
+    get_repo_info(repo_name="PrefectHQ/marvin")
